c6288bb5f13887d9e396c7a785860fa794fe04bf
Jeffery <jefferyhoekstra@hotmail.com>
2025-03-31 23:48:27 -0700

    upgrade

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+function loadComponents(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector(id).innerHTML = data;
+        }).catch(error => console.error('Error loading component:', error));
+}
+
+// LOAD COMPONENTS
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadComponents(".navbar", "./components/header.html");
+    loadComponents("footer", "./components/footer.html");
+    loadComponents(".projectsNav", "./components/projectsNav.html");
+});
